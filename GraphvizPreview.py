6cf269b7cacf9d34639eff057aff7656d37862b0
Andrew Robinson <awrobins@gmail.com>
2015-02-19 15:34:45 -0500

    Update GraphvizPreview.py

--- GraphvizPreview.py
+++ GraphvizPreview.py
@@ -4,28 +4,36 @@ import os
 import platform
 
 try:  # python 3
-    from .helpers import surroundingGraphviz, graphvizPDF, graphvizPNG, ENVIRON
+    from .helpers import surroundingGraphviz, graphvizDot, createTempFile, ENVIRON
 except ValueError:  # python 2
-    from helpers import surroundingGraphviz, graphvizPDF, graphvizPNG, ENVIRON
+    from helpers import surroundingGraphviz, graphvizDot, createTempFile, ENVIRON
 
 
 class GraphvizPreviewCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         sel = self.view.sel()[0]
 
-        if not sel.empty():
-            code = self.view.substr(sel).strip()
+        code = None
+        if 'DOT.tmLanguage' in self.view.settings().get('syntax'):
+            filename = self.view.file_name()
         else:
-            code = surroundingGraphviz(
-                self.view.substr(sublime.Region(0, self.view.size())),
-                sel.begin()
-            )
+            if not sel.empty():
+                code = self.view.substr(sel).strip()
+            else:
+                code = surroundingGraphviz(
+                    self.view.substr(sublime.Region(0, self.view.size())),
+                    sel.begin()
+                )
+            if not code:
+                sublime.error_message('Graphviz: Please place cursor in graphviz code before running')
+                return
+            filename = createTempFile(code)
 
-        if not code:
-            sublime.error_message('Graphviz: Please place cursor in graphviz code before running')
-            return
+        # TODO: allow user to choose which output type to use?
+        final_filename = graphvizDot(filename, 'png')
 
-        final_filename = graphvizPNG(code)
+        if code is not None:
+            os.unlink(filename)            
 
         try:
             if platform.system() == 'Windows':
@@ -35,5 +43,3 @@ class GraphvizPreviewCommand(sublime_plugin.TextCommand):
         except Exception as e:
             sublime.error_message('Graphviz: Could not open file, ' + str(e))
             raise e
-
-
